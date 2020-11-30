4e7c3a93f692874638e7f79757fd48c8359459ac
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-11-30 23:16:46 +0530

    - updated controllers

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,27 @@
+// import User from "../models/user";
+// import RequestError from "../middlewares/request-error";
+import {NextFunction, Request, RequestHandler, Response} from "express";
+// import {validationResult} from "express-validator";
+
+export const getUsers: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+};
+
+export const getUserById: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+};
+
+export const signUp: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    res.json({
+        "status": "TS works bitch"
+    });
+};
+
+export const login: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+};
+
+export const editUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+};
+
+export const deleteUser: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+};
+
+
