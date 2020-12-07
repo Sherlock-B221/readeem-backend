54b84ecc25dbdefe03e9fcae502c1b969f3f5dc9
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-12-07 16:58:22 +0530

    - modified check-auth.ts

--- src/middlewares/check-auth.ts
+++ src/middlewares/check-auth.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import RequestError from './request-error';
 import AccessBlackList from '../models/access_blacklist';
+import RefreshRevoked from '../models/refresh_revoke';
 import {NextFunction, Request, Response} from 'express';
 import {DecodedToken} from "../interfaces/decoded_token";
 
@@ -9,25 +10,59 @@ module.exports = async (req: Request, res: Response, next: NextFunction) => {
         return next();
     }
     try {
-        const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
-        if (!token) {
-            throw new RequestError('Authentication failed! token does not exist', 404);
+        const accessToken: string = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+        const refreshToken: string = req.body.refreshToken;
+        if (!accessToken && !refreshToken) {
+            throw new RequestError('Authentication failed! Access and Refresh Tokens does not exist', 401);
         }
         let tokenBlacklisted = await AccessBlackList.findOne({
-            "token": token
+            "accessToken": accessToken
         }) != null;
-
+        let refreshRevoked = await RefreshRevoked.findOne({
+            "refreshToken": refreshToken
+        }) != null;
         if (!tokenBlacklisted) {
-            const decodedToken = jwt.verify(token, process.env.JWT_KEY);
-            req.userData = {userId: (decodedToken as DecodedToken).userId};
+            const decodedToken = jwt.verify(accessToken, process.env.JWT_KEY);
+            req.userData = {userId: (decodedToken as DecodedToken).userId, email: (decodedToken as DecodedToken).email};
+            req.isValid = true;
             next();
-
+        } else if (!refreshRevoked) {
+            const decodedRefreshToken = jwt.verify(refreshToken, process.env.JWT_KEY);
+            await RefreshRevoked.create({refreshToken});
+            let userId = (decodedRefreshToken as DecodedToken).userId;
+            let email = (decodedRefreshToken as DecodedToken).email;
+            req.userData = {
+                userId,
+                email
+            };
+            req.isValid = false;
+            let newAccessToken,newRefreshToken;
+            try {
+                newAccessToken = jwt.sign(
+                    {userId: userId, email: email},
+                    process.env.JWT_KEY, {
+                        expiresIn: '6hr' // expires in 2d
+                    }
+                );
+                newRefreshToken = jwt.sign(
+                    {userId: userId, email: email},
+                    process.env.JWT_KEY, {
+                        expiresIn: '30d' // expires in 2d
+                    }
+                );
+            } catch (err) {
+                const error = new RequestError('Authentication flow failed, please try again later.', 500, err);
+                return next(error);
+            }
+            req.accessToken = newAccessToken;
+            req.refreshToken = newRefreshToken;
+            next();
         } else {
             const error = new RequestError('Authentication failed!', 403);
             return next(error);
         }
     } catch (err) {
-        const error = new RequestError("Token not sent!", 403);
+        const error = new RequestError(`Error with token. Info: ${err}`, 403);
         return next(error);
     }
 };
