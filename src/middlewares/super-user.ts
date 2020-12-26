22fec278d2ec2a8251cfe7c95a8dc9124da8a840
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-12-26 17:49:35 +0530

    - made a super user middleware

---
+++ src/middlewares/super-user.ts
@@ -0,0 +1,20 @@
+import {NextFunction, Request, Response} from "express";
+import RequestError from "./request-error";
+
+const checkSuperUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const superSecretKey: string = req.body.superSecretKey;
+        if (superSecretKey === process.env.SUPER_SECRET_KEY) {
+            next();
+        } else {
+            const error = new RequestError(`Not super user!!!`, 403);
+            return next(error);
+        }
+
+    } catch (e) {
+        const error = new RequestError(`Error occurred while checking super user.`, 403);
+        return next(error);
+    }
+}
+
+export default checkSuperUser;
