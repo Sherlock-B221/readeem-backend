af6e2ee1e51695944f2accf6a7ef0c2264529972
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-12-26 17:49:58 +0530

    - made item routes and controllers

---
+++ src/controllers/item-controller.ts
@@ -0,0 +1,20 @@
+// import Item from "../models/item";
+// import RequestError from "../middlewares/request-error";
+import {NextFunction, Request, RequestHandler, Response} from "express";
+// import {validationResult} from "express-validator";
+
+export const getAllItems: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+export const createItem: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+export const getItemById: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+export const editItem: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+
+};
--- src/routes/item-routes.ts
+++ src/routes/item-routes.ts
@@ -1,16 +1,41 @@
 import * as express from 'express';
 import {check} from 'express-validator';
-import {dummyRoute} from '../controllers/user-controller.js'
+import checkAuth from "../middlewares/check-auth";
+import {createItem, editItem, getAllItems, getItemById} from "../controllers/item-controller";
+import checkSuperUser from "../middlewares/super-user";
 
 const router = express.Router();
 
-router.post('/addUserToDb'
+router.post('/post'
     , [
-        check('email')
+        check('name')
+            .not().isEmpty(),
+        check('description')
+            .not().isEmpty(),
+        check('price')
+            .not().isEmpty(),
+        check('discountedRewardPrice')
+            .not().isEmpty(),
+        check('tags')
+            .not().isEmpty(),
+        check('couponCode')
+            .not().isEmpty(),
+        check('sellerName')
+            .not().isEmpty(),
+        check('sellerNumber')
+            .not().isEmpty(),
+        check('sellerEmail')
             .isEmail(),
+        check('categories')
+            .not().isEmpty(),
     ]
-    , dummyRoute
+    , checkAuth, checkSuperUser, createItem
 );
 
+router.get('/get/all', checkAuth, getAllItems);
+
+router.get('/get/one/:id', checkAuth, getItemById);
+
+router.patch('/patch', checkSuperUser, editItem);
 
 export default router;
