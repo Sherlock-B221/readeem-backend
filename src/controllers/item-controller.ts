46fbf7835885dd799f8f07176d34ccf1882a0a7f
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-01-12 00:51:12 +0530

    - updated item route and controller

--- src/controllers/item-controller.ts
+++ src/controllers/item-controller.ts
@@ -5,6 +5,7 @@ import {generateCrypto} from "../utils/generate-crypto";
 import {decryptCrypto} from "../utils/decrypt-crypto";
 import Item from "../models/item";
 import {checkTokens} from "../utils/check-tokens";
+import {IItem} from "../interfaces/item-interface";
 
 export const getAllItems: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -18,23 +19,73 @@ export const getAllItems: RequestHandler = async (req: Request, res: Response, n
 };
 
 export const createItem: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    validate(req, next)
-    const {couponCode} = req.body;
-    const encryptedCode = generateCrypto(couponCode)
-
+    validate(req, next);
+    const {
+        couponCode,
+        name,
+        description,
+        sellerName,
+        sellerEmail,
+        sellerNumber,
+        discountedRewardPrice,
+        tags,
+        categories,
+        price,
+    } = req.body;
+    let itemImg;
+    try {
+        itemImg = req.file.path;
+    } catch (err) {
+        console.log(err);
+        const error = new RequestError(err.message, err.code, err);
+        return next(error);
+    }
+    itemImg = 'https://win75.herokuapp.com/' + itemImg;
+    try {
+        const encryptedCode = generateCrypto(couponCode);
+        const newItem = new Item({
+            itemImg,
+            name,
+            description,
+            sellerName,
+            sellerEmail,
+            sellerNumber,
+            discountedRewardPrice,
+            tags,
+            categories,
+            price,
+            couponCode: encryptedCode
+        });
+        await newItem.save();
+        await res.json({
+            "status": "success",
+            newItem
+        });
+    } catch (err) {
+        const error = new RequestError("Error in adding item.", 400, err);
+        next(error);
+    }
 };
 
 export const getItemCouponCode: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    const encryptedCode = req.params.id;
-    if (!encryptedCode) {
-        const error = new RequestError('Code not sent!', 404);
-        next(error);
-    }
-    try{
+    const itemId = req.params.id;
+    let item: IItem;
+    try {
+        item = await Item.findById(itemId).lean();
+        if (!item) {
+            const error = new RequestError("Item with this id, doesn't exist.", 404);
+            next(error);
+        }
+        const encryptedCode = item.couponCode;
+        if (!encryptedCode) {
+            const error = new RequestError('Code not sent!', 404);
+            next(error);
+        }
         const decryptedCode = decryptCrypto(encryptedCode);
-        res.json({});
-    }catch(e) {
-        const error = new RequestError('Error in decrypting code!', 400);
+        const changedTokenPair = checkTokens(req.isAccessTokenValid, req.refreshToken, req.accessToken);
+        await res.json({"status": "success", decryptedCode, ...changedTokenPair});
+    } catch (e) {
+        const error = new RequestError('Error in getting decrypted code!', 400);
         next(error);
     }
 };
@@ -56,19 +107,67 @@ export const getItemById: RequestHandler = async (req: Request, res: Response, n
 };
 
 export const editItem: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-
+    const itemId = req.params.id;
+    let item: IItem;
+    try {
+        const {
+            couponCode,
+            name,
+            description,
+            sellerName,
+            sellerEmail,
+            sellerNumber,
+            discountedRewardPrice,
+            tags,
+            categories,
+            price,
+        } = req.body;
+        let itemImg;
+        try {
+            itemImg = req.file.path;
+        } catch (err) {
+            console.log(err);
+            const error = new RequestError(err.message, err.code, err);
+            return next(error);
+        }
+        itemImg = 'https://win75.herokuapp.com/' + itemImg;
+        item = await Item.findById(itemId);
+        if (!item) {
+            const error = new RequestError("Item with this id, doesn't exist.", 404);
+            next(error);
+        }
+        item.couponCode = couponCode;
+        item.name = name;
+        item.tags = tags;
+        item.categories = categories;
+        item.price = price;
+        item.itemImg = itemImg;
+        item.sellerEmail = sellerEmail;
+        item.sellerName = sellerName;
+        item.sellerNumber = sellerNumber;
+        item.description = description;
+        item.discountedRewardPrice = discountedRewardPrice;
+        await item.save();
+        res.status(200).json({
+            "status":"success",
+            item
+        });
+    } catch (err) {
+        const error = new RequestError('Error in editing item!!', 400);
+        next(error);
+    }
 };
 
 export const deleteItem: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id;
     try {
-        await Item.deleteOne({_id:id});
+        await Item.deleteOne({_id: id});
         await res.json({
             "status": "success",
             "message": "Item deleted successfully,if it existed."
         });
     } catch (e) {
-        const error = new RequestError("Error in deleting item", 400,e);
+        const error = new RequestError("Error in deleting item", 400, e);
         next(error);
     }
 };
--- src/routes/item-routes.ts
+++ src/routes/item-routes.ts
@@ -2,14 +2,17 @@ import * as express from 'express';
 import {check} from 'express-validator';
 import checkAuth from "../middlewares/check-auth";
 import {
-        createItem,
-        deleteItem,
-        editItem,
-        getAllItems,
-        getItemById,
-        getItemCouponCode, itemFullSearch, itemFuzzySearch
+    createItem,
+    deleteItem,
+    editItem,
+    getAllItems,
+    getItemById,
+    getItemCouponCode,
+    itemFullSearch,
+    itemFuzzySearch
 } from "../controllers/item-controller";
 import checkSuperUser from "../middlewares/super-user";
+import {singleFileUpload} from "../middlewares/single-file-upload";
 
 const router = express.Router();
 
@@ -35,21 +38,44 @@ router.post('/post'
             .isEmail(),
         check('categories')
             .not().isEmpty(),
+        singleFileUpload('uploads/itemImages', 'itemImg')
     ]
-    , checkAuth, checkSuperUser, createItem
+    , checkSuperUser, createItem
 );
 
 router.get('/get/all', checkAuth, getAllItems);
 
 router.get('/get/one/:id', checkAuth, getItemById);
 
-router.get('/get/search',checkAuth,itemFullSearch);
+router.get('/get/search', checkAuth, itemFullSearch);
 
-router.get('/get/fuzzy',checkAuth,itemFuzzySearch);
+router.get('/get/fuzzy', checkAuth, itemFuzzySearch);
 
-router.get('/get/couponCode/:id',checkAuth,getItemCouponCode);
+router.get('/get/couponCode/:id', checkAuth, getItemCouponCode);
 
-router.patch('/patch/:id', checkSuperUser, editItem);
+router.patch('/patch/:id', [
+    check('name')
+        .not().isEmpty(),
+    check('description')
+        .not().isEmpty(),
+    check('price')
+        .not().isEmpty(),
+    check('discountedRewardPrice')
+        .not().isEmpty(),
+    check('tags')
+        .not().isEmpty(),
+    check('couponCode')
+        .not().isEmpty(),
+    check('sellerName')
+        .not().isEmpty(),
+    check('sellerNumber')
+        .not().isEmpty(),
+    check('sellerEmail')
+        .isEmail(),
+    check('categories')
+        .not().isEmpty(),
+    singleFileUpload('uploads/itemImages', 'itemImg')
+], checkSuperUser, editItem);
 
 router.delete('/delete/:id', checkSuperUser, deleteItem);
 
